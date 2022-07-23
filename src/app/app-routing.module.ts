b34db27f1bea59ad551108b8b041998d88ade8df
Guilherme Mota <metatronmota@gmail.com>
2022-07-23 02:20:05 -0300

    to do list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  BrowserModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
